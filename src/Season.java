869b3f966f25bfa6cbb6230989eeda2e5a12236e
Nali1234-lab <naliahmadi00@gmail.com>
2025-11-07 12:19:03 +0100

    Season-klassenv0.

--- src/Season.java
+++ src/Season.java
@@ -1,2 +1,32 @@
+import java.util.ArrayList;
+
 public class Season {
+    private int seasonNumber;
+    private ArrayList<Episode> episods= new ArrayList<>();
+
+    public Season(int seasonNumber, ArrayList<Episode> episodes){
+        this.seasonNumber= seasonNumber;
+        this.episods= episodes;
+
+    }
+
+
+    public int getSeasonNumber() {
+        return seasonNumber;
+    }
+
+    public void setSeasonNumber(int seasonNumber) {
+        this.seasonNumber = seasonNumber;
+    }
+
+    public ArrayList<Episode> getEpisods() {
+        return episods;
+    }
+
+    public void setEpisods(ArrayList<Episode> episods) {
+        this.episods = episods;
+    }
+
+
+
 }
