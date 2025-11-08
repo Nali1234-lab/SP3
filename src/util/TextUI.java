169cd65c88d3546625073d779409dca900b23ec5
Nali1234-lab <naliahmadi00@gmail.com>
2025-11-08 09:17:31 +0100

    textUI-v0-Nali

--- src/util/TextUI.java
+++ src/util/TextUI.java
@@ -1,4 +1,25 @@
 package util;
 
+import java.util.Scanner;
+
 public class TextUI {
+    private static Scanner sc = new Scanner(System.in);
+
+    public String getUserInput(String prompt){
+        System.out.println(prompt);
+        return sc.nextLine();
+    }
+    public void showMessage(String message){
+        System.out.println(message);
+    }
+    public String promptForUsername(){
+        System.out.println("Username: ");
+        return sc.nextLine();// indtil nu
+
+    }
+    public String promptForPassword(){
+        System.out.println("Password: ");
+        return sc.nextLine(); // indtil nu
+
+    }
 }
