1871323d245100e26746e77f4d25947eceef95f9
Nali1234-lab <naliahmadi00@gmail.com>
2025-11-08 08:15:21 +0100

    episode-v0-Nali

--- src/Episode.java
+++ src/Episode.java
@@ -1,2 +1,23 @@
 public class Episode {
+    private String title;
+    private int duration;
+    public Episode(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+
+
+
+    public void play() {
+        System.out.println("Playing episode: " + title);// bruge textui. senere
+
+    }
+
+    public void pause() {
+        System.out.println("Episode paused: " + title); // bruge textui. senere
+    }
+
+
+
 }
