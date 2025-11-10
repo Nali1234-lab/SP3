3085ac94258f21b4f18c2c876a81d5891546d2ab
Gabbyskii <gkadiga1@gmail.com>
2025-11-10 10:16:04 +0100

    fileio class changes

--- src/util/Fileio.java
+++ src/util/Fileio.java
@@ -1,4 +1,24 @@
 package util;
 
-public class Fileio {
+import java.util.List;
+
+    public class FileIO {
+        //attribut
+        private String userFile;
+
+        //constructor
+        public FileIO() {
+            this.userFile = "users.txt"; //vores filename kunne ændres?
+        }
+
+        //metoder
+        public void saveUsers(List<User> users) {
+            //skal gemme brugere til file
+        }
+
+        public List<User> loadUsers() {
+            //skal indlæse brugere fra file
+            return null;
+        }
+    }
 }
