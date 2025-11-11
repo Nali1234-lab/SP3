8ce9587b997f108c07ba9cd0bc1fb7a4fa1dc672
MalakKh1 <malakkh166@gmail.com>
2025-11-11 11:17:19 +0100

    j

--- src/User.java
+++ src/User.java
@@ -1,2 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    private String username;
+    private String password;
+    private List<Media> watchedMedia;
+    private List<Media> savedMedia;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.watchedMedia = new ArrayList<>();
+        this.savedMedia = new ArrayList<>();
+    }
+
+    public void login() {
+        // Implementer senere
+    }
+
+    public void register() {
+        // Implementer senere
+    }
+
+    public void addToWatched(Media media) {
+        watchedMedia.add(media);
+    }
+
+    public void addToSaved(Media media) {
+        savedMedia.add(media);
+    }
+
+    // Gettere
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
+    public List<Media> getWatchedMedia() { return watchedMedia; }
+    public List<Media> getSavedMedia() { return savedMedia; }
 }
